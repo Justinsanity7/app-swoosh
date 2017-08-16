3411751a077f2827184c5d8103d25d9362001291
Justin Blackhurst <justin.blackhurst7@gmail.com>
2017-08-16 17:27:11 -0600

    through lesson 6

---
+++ app-swoosh/app-swoosh/View/Controller/WelcomeVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Justin Blackhurst on 8/15/17.
+//  Copyright © 2017 Justin Blackhurst. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+        
+    }
+    
+}
+
